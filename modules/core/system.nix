1fd746d969d2a65948bb67c8dbb2b4e329ea7019
IcyMagicCrystal <isaac@grinberg.net>
2026-04-14 08:32:12 -0400

    Add gcc

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -80,6 +80,7 @@
         git
         vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         wget
+        gcc
         curl
         gh
         neovim
